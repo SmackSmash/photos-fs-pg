5191f9b9d42ccefd1bffe9003566fbd7420bd6f8
Dan Moffat <danmoffat@gmail.com>
2025-08-01 00:54:11 +0100

    Added super basic express setup

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log(process.env.MESSAGE);
+import express from 'express';
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
