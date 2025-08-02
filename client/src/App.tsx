4ac3c71143663befe3ed767b3cf68e4d25782c5e
Dan Moffat <danmoffat@gmail.com>
2025-08-02 18:22:09 +0100

    Added dark mode via shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from './components/ui/theme-provider';
 import UsersList from './components/UsersList';
 
 const App = () => {
   return (
-    <div className='container mx-auto p-2'>
-      <UsersList />
-    </div>
+    <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+      <div className='container mx-auto p-2'>
+        <UsersList />
+      </div>
+    </ThemeProvider>
   );
 };
 
