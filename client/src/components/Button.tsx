3baec614f67258cc7540eb4d8b96a69eeab34211
Dan Moffat <danmoffat@gmail.com>
2025-08-02 02:28:18 +0100

    Added className passthrough to Button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,10 +1,18 @@
 import type { FC, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-const Button: FC<{ children: ReactNode; [x: string]: unknown }> = ({ children, ...rest }) => {
+const Button: FC<{ children: ReactNode; className?: string; [x: string]: unknown }> = ({
+  children,
+  className,
+  ...rest
+}) => {
   return (
     <button
       {...rest}
-      className='focus-visible:outline-blue box-border flex h-10 cursor-pointer items-center justify-center rounded-md border border-zinc-500 bg-zinc-800 px-3.5 py-0 font-medium text-zinc-50 outline-0 select-none hover:bg-zinc-700 focus-visible:outline-2 focus-visible:-outline-offset-1 active:bg-zinc-600'
+      className={twMerge(
+        'focus-visible:outline-blue box-border flex h-10 cursor-pointer items-center justify-center rounded-md border border-zinc-500 bg-zinc-800 px-3.5 py-0 font-medium text-zinc-50 outline-0 select-none hover:bg-zinc-700 focus-visible:outline-2 focus-visible:-outline-offset-1 active:bg-zinc-600',
+        className || ''
+      )}
     >
       {children}
     </button>
