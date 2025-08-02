b6b359560837f649cb5e6c89e9ea1ad2878fb74c
Dan Moffat <danmoffat@gmail.com>
2025-08-02 19:18:57 +0100

    Added a little more state

--- client/src/store/slices/usersSlice.ts
+++ client/src/store/slices/usersSlice.ts
@@ -2,10 +2,14 @@ import { createSlice } from '@reduxjs/toolkit';
 import { type User } from '@/types';
 
 type UsersState = {
+  isLoading: boolean;
+  error: null | string;
   data: User[];
 };
 
 const initialState: UsersState = {
+  isLoading: false,
+  error: null,
   data: []
 };
 
