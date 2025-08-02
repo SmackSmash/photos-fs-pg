31a41fab8a698b7fd88178331418454a396df24a
Dan Moffat <danmoffat@gmail.com>
2025-08-03 00:58:42 +0100

    Added a spinner

--- client/src/components/UsersList.tsx
+++ client/src/components/UsersList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, type FC } from 'react';
 import { useAppDispatch, useAppSelector } from '@/store';
 import { fetchUsers } from '@/store';
+import { Spinner } from './ui/shadcn-io/spinner';
 
 const UsersList: FC = () => {
   const { isLoading, error, data } = useAppSelector(({ users }) => users);
@@ -10,7 +11,12 @@ const UsersList: FC = () => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <div>
+        <Spinner variant='ring' />
+      </div>
+    );
 
   if (error) return <div>{error.message}</div>;
 
