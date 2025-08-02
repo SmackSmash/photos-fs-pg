3e12b5c720f9883cb1c45993ead0bb1fe7a6564d
Dan Moffat <danmoffat@gmail.com>
2025-08-02 14:57:13 +0100

    Fleshed out DB schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,9 @@
 import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 
-export const photosTable = pgTable('photos', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  fileName: varchar({ length: 255 }).notNull(),
-  title: varchar({ length: 255 }).notNull(),
-  alt: varchar({ length: 255 }).notNull()
-});
-
 export const usersTable = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  firstName: varchar({ length: 255 }).notNull(),
+  secondName: varchar({ length: 255 }).notNull(),
   userName: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull(),
   password: varchar({ length: 255 }).notNull()
@@ -16,7 +11,16 @@ export const usersTable = pgTable('users', {
 
 export const albumsTable = pgTable('albums', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: integer().notNull(),
   albumName: varchar({ length: 255 }).notNull(),
   albumPhotos: varchar({ length: 255 }).notNull(),
   albumUser: varchar({ length: 255 }).notNull()
+});
+
+export const photosTable = pgTable('photos', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  albumId: integer().notNull(),
+  fileName: varchar({ length: 255 }).notNull(),
+  title: varchar({ length: 255 }).notNull(),
+  alt: varchar({ length: 255 }).notNull()
 });
