21c901c4d23cf16f271c35aa2d97b888087de068
Dan Moffat <danmoffat@gmail.com>
2025-08-02 19:01:43 +0100

    Fleshed out users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,28 @@
 import { Router } from 'express';
+import db from '../db/connect';
+import { usersTable as users } from '../db/schema';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Users route works');
+// @route   GET /users
+// @desc    Get all users
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const result = await db
+      .select({
+        id: users.id,
+        firstName: users.firstName,
+        secondName: users.secondName,
+        userName: users.userName,
+        email: users.email
+      })
+      .from(users);
+    console.log(result);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 export default router;
