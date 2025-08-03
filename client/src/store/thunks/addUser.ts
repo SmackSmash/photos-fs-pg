b1d457498dad67b2551f15120cdecf5e9727f794
Dan Moffat <danmoffat@gmail.com>
2025-08-03 19:08:43 +0100

    Refetch users when one is successfully added

--- client/src/store/thunks/addUser.ts
+++ client/src/store/thunks/addUser.ts
@@ -1,5 +1,6 @@
 import type { User } from '@/types';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import store, { fetchUsers } from '@/store';
 
 export const addUser = createAsyncThunk('users/add', async (user: User) => {
   const response = await fetch('http://localhost:3000/users', {
@@ -10,5 +11,6 @@ export const addUser = createAsyncThunk('users/add', async (user: User) => {
     body: JSON.stringify(user)
   });
   const data = await response.json();
+  store.dispatch(fetchUsers());
   return data;
 });
